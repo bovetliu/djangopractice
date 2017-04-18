f79e7b5ff9f00abc55301fe7f5d07657ffa1eed8
Bowei Liu <boweiliu@fortinet.com>
2017-04-18 11:19:29 -0700

    added test

--- poll2/tests.py
+++ poll2/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class RegularTest(TestCase):
+
+    def test_001(self):
+        self.assertTrue(1 == 1)
+
+    def test_002(self):
+        self.assertTrue(2 == 2)
+
+    def test_003(self):
+        self.assertTrue(3 == 3)
+
+    def test_004(self):
+        self.assertTrue(4 == 4)
+
